683b4c29da96065bae7ca115bc4c16b69f5fd1de
PIETRO FERNANDO RODRIGUES DE OLIVEIRA <pietro.froliveira@senacsp.edu.br>
2026-02-25 15:42:22 -0300

    atualizaçao atividades

---
+++ DadosNecessario/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DadosNecessario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblDataNasc = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.comboBoxCidade = new System.Windows.Forms.ComboBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.rbNaoBinario = new System.Windows.Forms.RadioButton();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFeminino = new System.Windows.Forms.RadioButton();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txtNumeroCadastrado = new System.Windows.Forms.TextBox();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeCompleto.Location = new System.Drawing.Point(36, 40);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(356, 22);
+            this.txtNomeCompleto.TabIndex = 0;
+            this.txtNomeCompleto.Text = "insira seu nome completo";
+            this.txtNomeCompleto.Leave += new System.EventHandler(this.txtNomeCompleto_Leave);
+            // 
+            // lblDataNasc
+            // 
+            this.lblDataNasc.AutoSize = true;
+            this.lblDataNasc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataNasc.Location = new System.Drawing.Point(33, 69);
+            this.lblDataNasc.Name = "lblDataNasc";
+            this.lblDataNasc.Size = new System.Drawing.Size(130, 16);
+            this.lblDataNasc.TabIndex = 1;
+            this.lblDataNasc.Text = "Data de Nascimento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(169, 65);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(99, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(33, 92);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(51, 16);
+            this.lblCidade.TabIndex = 3;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // comboBoxCidade
+            // 
+            this.comboBoxCidade.FormattingEnabled = true;
+            this.comboBoxCidade.Items.AddRange(new object[] {
+            "São paulo",
+            "Rio de Janeiro",
+            "Manaus",
+            "Salvador",
+            "Brasilia"});
+            this.comboBoxCidade.Location = new System.Drawing.Point(140, 87);
+            this.comboBoxCidade.Name = "comboBoxCidade";
+            this.comboBoxCidade.Size = new System.Drawing.Size(128, 21);
+            this.comboBoxCidade.TabIndex = 4;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.rbNaoBinario);
+            this.groupBox.Controls.Add(this.rbMasculino);
+            this.groupBox.Controls.Add(this.rbFeminino);
+            this.groupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox.Location = new System.Drawing.Point(27, 116);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(501, 126);
+            this.groupBox.TabIndex = 5;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Gênero";
+            // 
+            // rbNaoBinario
+            // 
+            this.rbNaoBinario.AutoSize = true;
+            this.rbNaoBinario.Location = new System.Drawing.Point(371, 61);
+            this.rbNaoBinario.Name = "rbNaoBinario";
+            this.rbNaoBinario.Size = new System.Drawing.Size(96, 20);
+            this.rbNaoBinario.TabIndex = 2;
+            this.rbNaoBinario.TabStop = true;
+            this.rbNaoBinario.Text = "Não Bínario";
+            this.rbNaoBinario.UseVisualStyleBackColor = true;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(206, 61);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(86, 20);
+            this.rbMasculino.TabIndex = 1;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFeminino
+            // 
+            this.rbFeminino.AutoSize = true;
+            this.rbFeminino.Location = new System.Drawing.Point(40, 61);
+            this.rbFeminino.Name = "rbFeminino";
+            this.rbFeminino.Size = new System.Drawing.Size(80, 20);
+            this.rbFeminino.TabIndex = 0;
+            this.rbFeminino.TabStop = true;
+            this.rbFeminino.Text = "Feminino";
+            this.rbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(27, 272);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(107, 50);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtNumeroCadastrado
+            // 
+            this.txtNumeroCadastrado.Location = new System.Drawing.Point(36, 12);
+            this.txtNumeroCadastrado.Name = "txtNumeroCadastrado";
+            this.txtNumeroCadastrado.Size = new System.Drawing.Size(353, 20);
+            this.txtNumeroCadastrado.TabIndex = 7;
+            this.txtNumeroCadastrado.Text = "Número Cadastrado";
+            this.txtNumeroCadastrado.Leave += new System.EventHandler(this.txtNumeroCadastrado_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumeroCadastrado);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.comboBoxCidade);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblDataNasc);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblDataNasc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.ComboBox comboBoxCidade;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.RadioButton rbNaoBinario;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.RadioButton rbFeminino;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtNumeroCadastrado;
+    }
+}
+
---
+++ calcularidade/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace calcularidade
+{
+    partial class frmCalcularIdade
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAnoNasc = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtAnoNasc = new System.Windows.Forms.TextBox();
+            this.lblAnoAtual = new System.Windows.Forms.Label();
+            this.txtAnoAtual = new System.Windows.Forms.TextBox();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAnoNasc
+            // 
+            this.lblAnoNasc.AutoSize = true;
+            this.lblAnoNasc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnoNasc.ForeColor = System.Drawing.Color.White;
+            this.lblAnoNasc.Location = new System.Drawing.Point(167, 48);
+            this.lblAnoNasc.Name = "lblAnoNasc";
+            this.lblAnoNasc.Size = new System.Drawing.Size(177, 24);
+            this.lblAnoNasc.TabIndex = 0;
+            this.lblAnoNasc.Text = "Ano de Nascimento";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtAnoNasc
+            // 
+            this.txtAnoNasc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoNasc.Location = new System.Drawing.Point(144, 85);
+            this.txtAnoNasc.Name = "txtAnoNasc";
+            this.txtAnoNasc.Size = new System.Drawing.Size(223, 29);
+            this.txtAnoNasc.TabIndex = 2;
+            // 
+            // lblAnoAtual
+            // 
+            this.lblAnoAtual.AutoSize = true;
+            this.lblAnoAtual.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnoAtual.ForeColor = System.Drawing.Color.White;
+            this.lblAnoAtual.Location = new System.Drawing.Point(209, 152);
+            this.lblAnoAtual.Name = "lblAnoAtual";
+            this.lblAnoAtual.Size = new System.Drawing.Size(92, 24);
+            this.lblAnoAtual.TabIndex = 3;
+            this.lblAnoAtual.Text = "Ano Atual";
+            // 
+            // txtAnoAtual
+            // 
+            this.txtAnoAtual.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoAtual.Location = new System.Drawing.Point(144, 191);
+            this.txtAnoAtual.Name = "txtAnoAtual";
+            this.txtAnoAtual.Size = new System.Drawing.Size(222, 29);
+            this.txtAnoAtual.TabIndex = 4;
+            this.txtAnoAtual.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAnoAtual_KeyPress);
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.ForeColor = System.Drawing.Color.White;
+            this.lblIdade.Location = new System.Drawing.Point(243, 267);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(24, 25);
+            this.lblIdade.TabIndex = 5;
+            this.lblIdade.Text = "0";
+            // 
+            // frmCalcularIdade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepPink;
+            this.ClientSize = new System.Drawing.Size(518, 495);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.txtAnoAtual);
+            this.Controls.Add(this.lblAnoAtual);
+            this.Controls.Add(this.txtAnoNasc);
+            this.Controls.Add(this.lblAnoNasc);
+            this.Name = "frmCalcularIdade";
+            this.Text = "Calcular Idade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAnoNasc;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtAnoNasc;
+        private System.Windows.Forms.Label lblAnoAtual;
+        private System.Windows.Forms.TextBox txtAnoAtual;
+        private System.Windows.Forms.Label lblIdade;
+    }
+}
+
